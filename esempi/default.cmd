2d5bdab9317959906f25427fd578b4e4a5e1b4b6
Stefan <stefan.ferro5@gmail.com>
2026-03-12 14:40:11 +0100

    Removed dir 'esempi'

--- esempi/default.cmd
+++
@@ -1,5 +0,0 @@
-@echo off
-echo window.CMD_PARAMS = { }; > ..\params.js
-start ..\index.html
-echo Caricato scenario: DEFAULT (Dati originali CSV)
-pause
--- params.js
+++ params.js
@@ -1,3 +1,1 @@
-// Questo file viene sovrascritto dai file .cmd nella cartella esempi.
-// Se vuoto, la dashboard carica lo scenario Standard (Default).
-window.CMD_PARAMS = {};
+window.CMD_PARAMS = { }; 
